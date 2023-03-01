e388859233002139c3692d0f6c246fd370b519b0
MATEKUNA <kuna.mate@parlament.hu>
2023-03-01 10:53:07 +0100

    Navigate between pages, Link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
+import Products from './pages/Products';
+
+// same function with createRoutesFromElements
+
+/* const routeDefinitions = createRoutesFromElements(
+	<Route>
+		<Route path='/' element={<HomePage />} />
+		<Route path='/products' element={<Products />} />
+	</Route>
+); */
+
+// const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
 	{
 	  path: '/',
 	  element: <HomePage />,
 	},
+	{
+		path: '/products',
+		element: <Products />
+	}
   ]);
 
 function App() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+// using regular a href would cause the app to send unneccessary request and refresh page when routing to page: Link solves the problem
 const HomePage = () => {
-    return <h1>Home Page</h1>
+    return (
+        <>
+            <h1>Home Page</h1>
+            <p> Go to <Link to='/products'> products page </Link></p>
+        </>
+    )
 }
 
 export default HomePage;
