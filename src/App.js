2b9ff6e964afa791093b7b58e67829cdafba0ffc
MATEKUNA <kuna.mate@parlament.hu>
2023-03-01 10:24:32 +0100

    homePage basic router

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+
+const router = createBrowserRouter([
+	{
+	  path: '/',
+	  element: <HomePage />,
+	},
+  ]);
+
+function App() {
+  	return <RouterProvider router={router} />
+}
+
+export default App;
